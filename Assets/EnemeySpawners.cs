e470fea07d316a36119dc1de33b81c1f3d65a77b
Tej-Sharma <tejassharma08@gmail.com>
2023-04-23 16:26:13 -0400

    fix

--- Assets/EnemeySpawners.cs
+++ Assets/EnemeySpawners.cs
@@ -8,9 +8,10 @@ public class EnemeySpawners : MonoBehaviour
 
     // create 3 enemy prefabs
     [SerializeField] private GameObject[] enemies;
-
     // spawn rate
-    [SerializeField] private float spawnRate = 1f;
+    [SerializeField] private float spawnRate = 0.5f;
+
+    private float lastTimeSpawned = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -21,18 +22,44 @@ public class EnemeySpawners : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      // spawn rate
-      if (Time.time % spawnRate < 0.01f) {
-        // spawn enemy
+      // spawn enemy
+      if (Time.time - lastTimeSpawned > spawnRate) {
         SpawnEnemy();
-      }  
+        lastTimeSpawned = Time.time;
+      }
+    }
+
+    Vector3 GetSpawnPositionOutsideViewBox() {
+      // get random position
+      float randomX = Random.Range(-10f, 10f);
+      float randomY = Random.Range(-10f, 10f);
+
+      Vector3 randomPosition = new Vector3(randomX, randomY, 0f);
+
+      // get camera bounds
+      float camHeight = Camera.main.orthographicSize;
+      float camWidth = camHeight * Camera.main.aspect;
+
+      // check if random position is in camera
+      while (randomPosition.x < camWidth && randomPosition.x > -camWidth && randomPosition.y < camHeight && randomPosition.y > -camHeight) {
+        // get new random position
+        randomX = Random.Range(-10f, 10f);
+        randomY = Random.Range(-10f, 10f);
+
+        randomPosition = new Vector3(randomX, randomY, 0f);
+      }
+
+      return randomPosition;
+
     }
 
     void SpawnEnemy() {
       // get random enemy
       int randomEnemy = Random.Range(0, enemies.Length);
-      // get random position
-      Vector2 randomPosition = new Vector2(Random.Range(-8f, 8f), Random.Range(-4f, 4f));
+
+      // random positin
+      Vector3 randomPosition = GetSpawnPositionOutsideViewBox();  
+
       // spawn enemy
       Instantiate(enemies[randomEnemy], randomPosition, Quaternion.identity);
     }
